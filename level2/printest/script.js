4baf2529fa2fabfdd482f6e0c549ba093dbd23ec
Nightimator <rojenchakradhar72@gmail.com>
2024-04-17 16:17:12 +0545

    printest

---
+++ level2/printest/script.js
@@ -0,0 +1,9 @@
+let image = [
+    {name:"Man in rain with umbrella", image:"https://images.unsplash.com/photo-1712781797301-ec9ee12b52b4?q=80&w=1529&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Waterfall", image:"https://images.unsplash.com/photo-1712971578942-8d44b2d77143?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"Sunset in ocean", image:"https://images.unsplash.com/photo-1713236252021-00d5792e69e5?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"", image:""},
+    {name:"", image:""},
+    {name:"", image:""},
+    {name:"", image:""}
+]
